842f99fcca112b95898682dc486e3fdbf5d6d8a7
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-10-23 00:36:00 +0330

    update codebase

---
+++ android/app/build/generated/source/buildConfig/debug/com/sharedcrm/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sharedcrm;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sharedcrm";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from default config.
+  public static final String DEFAULT_REGION = "IR";
+  // Field from default config.
+  public static final String ORG_ID = "4e8bcd76-689f-4e3d-b0fb-2d7b7c1e383d";
+  // Field from default config.
+  public static final String SUPABASE_ANON_KEY = "YOUR_ANON_JWT";
+  // Field from default config.
+  public static final String SUPABASE_URL = "postgresql://postgres:gHsXUEKQykhplCyF@db.xujsxmayrlousahexxaj.supabase.co:5432/postgres";
+}
